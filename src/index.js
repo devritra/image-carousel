bde93c72373c04f1fae5ffc9b30b9ac60b5f8f4d
Aritra Saha <158503009+devritra@users.noreply.github.com>
2025-11-26 00:46:10 +0530

    Left and right navigation buttons are done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import './style.css';
+const frame = document.querySelector(".slider_frame");
+const slides = document.querySelectorAll(".carousel_image_holder");
+const slideArr = [...slides];
+let currentSlideIndex = 0;
+frame.addEventListener("click", (e)=>{
+    switch (e.target.dataset.action) {
+        case "scroll_left":
+            if(currentSlideIndex !== 0){
+                currentSlideIndex--;
+                slideArr[currentSlideIndex].scrollIntoView({
+                    behavior: 'smooth',
+                    inline: 'center'
+                })
+                break;
+            } else {
+                break;
+            }
+        case "scroll_right":
+            if(currentSlideIndex !== 4){
+                currentSlideIndex++;
+                slideArr[currentSlideIndex].scrollIntoView({
+                    behavior: 'smooth',
+                    inline: 'center'
+                })
+                break;
+            } else {
+                break;
+            }
+    }
+})
